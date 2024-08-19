0d89940545240bbf6c4e873cf7e84b983359430b
BNunez1013 <126496163+BNunez1013@users.noreply.github.com>
2024-08-19 14:31:52 -0400

    first commit

---
+++ client/src/pages/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/collection">Collection</Link>
+        </li>
+        <li>
+          <Link to="/account">Account</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
